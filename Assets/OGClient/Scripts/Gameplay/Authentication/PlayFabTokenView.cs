4676b6d7d3d12578e1ec2f95c4a348d5728c708e
Vlad Frolov <73vladfrolov73@gmail.com>
2025-06-16 02:02:56 +0300

    added token view

---
+++ Assets/OGClient/Scripts/Gameplay/Authentication/PlayFabTokenView.cs
@@ -0,0 +1,29 @@
+﻿using TMPro;
+using UniRx;
+using UnityEngine;
+using Zenject;
+namespace OGClient.Gameplay.Authentication
+{
+    public class PlayFabTokenView : MonoBehaviour
+    {
+
+        [SerializeField] private TextMeshProUGUI _tokenText;
+
+        private PlayFabAuthDataProxy _playFabAuthDataProxy;
+
+        [Inject]
+        public void Construct(PlayFabAuthDataProxy playFabAuthDataProxy)
+        {
+            _playFabAuthDataProxy = playFabAuthDataProxy;
+        }
+
+        private void Awake()
+        {
+            _playFabAuthDataProxy.PlayFabToken.Subscribe(data =>
+            {
+                _tokenText.text = string.IsNullOrEmpty(data) ? $"Token not available." : $"{data}";
+            }).AddTo(this);
+        }
+
+    }
+}
--- Assets/OGServer/Scripts/Matchmaking/MatchStartController.cs
+++ Assets/OGServer/Scripts/Matchmaking/MatchStartController.cs
@@ -11,11 +11,12 @@ namespace OGServer.Matchmaking
 
         public void PlayerJoined(PlayerRef player)
         {
-            Debug.Log($"Runner is server: {Runner.IsServer} | Active players count: {Runner.ActivePlayers.Count()} | Required players: {ConstantsModel.PLAYERS_PER_MATCH}");
+            Debug.Log($"Active players count: {Runner.ActivePlayers.Count()} | Required players: {ConstantsModel.PLAYERS_PER_MATCH}");
             if (!Runner.IsServer) return;
-            if (Runner.ActivePlayers.Count() < ConstantsModel.PLAYERS_PER_MATCH) return;
-
-            Runner.SceneManager.LoadScene(_gameplayScene, new NetworkLoadSceneParameters());
+            if (Runner.ActivePlayers.Count() == ConstantsModel.PLAYERS_PER_MATCH)
+            {
+                Runner.SceneManager.LoadScene(_gameplayScene, new NetworkLoadSceneParameters());
+            }
         }
 
     }
